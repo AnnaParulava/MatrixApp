fb39123c13f359ea69bd05b3d30082313b3e614d
AnnaParulava <ap554156@yandex.ru>
2024-04-21 22:32:00 +0300

    UI added

---
+++ app/src/main/java/com/kinopoisk/matrixapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.kinopoisk.matrixapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kinopoisk.matrixapp.ui.theme.MatrixAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MatrixAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "${add(2,2)}",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+    external fun stringFromJNI(): String
+    external fun add(x: Int, y: Int): Int
+//    external fun sub(x: Int, y: Int): Int
+//    external fun multiply(x: Int, y: Int): Int
+//    external fun divide(x: Int, y: Int): Int
+
+    companion object {
+        // Used to load the 'myfirstcppapp' library on application startup.
+        init {
+            System.loadLibrary("matrixapp")
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MatrixAppTheme {
+        Greeting("Android")
+    }
+}
